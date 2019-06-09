62c72f216882ba068f54779e006d9ffafc3ca211
saurabh0719 <saurabhpuj99@gmail.com>
2019-06-10 00:31:16 +0530

    update13

--- perspective_transform.cpp
+++ perspective_transform.cpp
@@ -1,1 +1,11 @@
-ljljlk
+//to get the 3*3 transformation matrix
+
+/*  Calculates a perspective transform from four pairs of the corresponding points.
+
+The function calculates the 3×3 matrix of a perspective transform so that:
+
+⎡⎣⎢⎢⎢tix′itiy′iti⎤⎦⎥⎥⎥=𝚖𝚊𝚙\_𝚖𝚊𝚝𝚛𝚒𝚡⋅⎡⎣⎢⎢xiyi1⎤⎦⎥⎥
+
+where
+
+dst(i)=(x′i,y′i),src(i)=(xi,yi),i=0,1,2,3 */
