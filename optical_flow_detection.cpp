47ecf83055eb76deedbdaebec8a85454de8f54e3
saurabh0719 <saurabhpuj99@gmail.com>
2019-06-09 12:44:02 +0530

    changes in main

--- optical_flow_detection.cpp
+++ optical_flow_detection.cpp
@@ -9,187 +9,188 @@ using namespace std;
 #define maxFeaturesW (maxFeatures/maxFeaturesH)
 #define Threshold 250000	// 5150000 for city drone video // 1250000 for beach highway drone video
 #define corner_count 4
+#define window 9
 
 /*******************GAUSSIAN FILTER****************************************************************/
 
-//Function to create Gaussian filter 
-void FilterCreation(double GKernel[][5]) 
-{ 
-    // intialising standard deviation to 1.0 
-    double sigma = 1.0; 
-    double r, s = 2.0 * sigma * sigma; 
-  
-    // sum is for normalization 
-    double sum = 0.0; 
-  
-    // generating 5x5 kernel 
-    for (int x = -2; x <= 2; x++) { 
-        for (int y = -2; y <= 2; y++) { 
-            r = sqrt(x * x + y * y); 
-            GKernel[x + 2][y + 2] = (exp(-(r * r) / s)) / (M_PI * s); 
-            sum += GKernel[x + 2][y + 2]; 
-        } 
-    } 
-  
-    // normalising the Kernel 
-    for (int i = 0; i < 5; ++i) 
-        for (int j = 0; j < 5; ++j) 
-            GKernel[i][j] /= sum; 
-} 
-  
+//Function to create Gaussian filter
+void FilterCreation(double GKernel[][5])
+{
+    // intialising standard deviation to 1.0
+    double sigma = 1.0;
+    double r, s = 2.0 * sigma * sigma;
+
+    // sum is for normalization
+    double sum = 0.0;
+
+    // generating 5x5 kernel
+    for (int x = -2; x <= 2; x++) {
+        for (int y = -2; y <= 2; y++) {
+            r = sqrt(x * x + y * y);
+            GKernel[x + 2][y + 2] = (exp(-(r * r) / s)) / (M_PI * s);
+            sum += GKernel[x + 2][y + 2];
+        }
+    }
+
+    // normalising the Kernel
+    for (int i = 0; i < 5; ++i)
+        for (int j = 0; j < 5; ++j)
+            GKernel[i][j] /= sum;
+}
+
 /**************************************************************************************************/
 
 /********************TRANSPOSE OF A MATRIX*********************************************************/
 
-// This function stores transpose of A[][] in B[][] 
-void transpose(int A[][N], int B[][N]) 
-{ 
-    int i, j; 
-    for (i = 0; i < N; i++) 
-        for (j = 0; j < N; j++) 
-            B[i][j] = A[j][i]; 
-} 
+// This function stores transpose of A[][] in B[][]
+void transpose(int A[][2], int B[][window])
+{
+    int i, j;
+    for (i = 0; i < N; i++)
+        for (j = 0; j < N; j++)
+            B[i][j] = A[j][i];
+}
 
 /**************************************************************************************************/
 
 /**************INVERSE OF A MATRIX*****************************************************************/
 
-// Function to get cofactor of A[p][q] in temp[][]. n is current 
-// dimension of A[][] 
-void getCofactor(int A[N][N], int temp[N][N], int p, int q, int n) 
-{ 
-    int i = 0, j = 0; 
-  
-    // Looping for each element of the matrix 
-    for (int row = 0; row < n; row++) 
-    { 
-        for (int col = 0; col < n; col++) 
-        { 
-            //  Copying into temporary matrix only those element 
-            //  which are not in given row and column 
-            if (row != p && col != q) 
-            { 
-                temp[i][j++] = A[row][col]; 
-  
-                // Row is filled, so increase row index and 
-                // reset col index 
-                if (j == n - 1) 
-                { 
-                    j = 0; 
-                    i++; 
-                } 
-            } 
-        } 
-    } 
-} 
-  
-/* Recursive function for finding determinant of matrix. 
+// Function to get cofactor of A[p][q] in temp[][]. n is current
+// dimension of A[][]
+void getCofactor(int A[2][2], int temp[2][2], int p, int q, int n)
+{
+    int i = 0, j = 0;
+
+    // Looping for each element of the matrix
+    for (int row = 0; row < n; row++)
+    {
+        for (int col = 0; col < n; col++)
+        {
+            //  Copying into temporary matrix only those element
+            //  which are not in given row and column
+            if (row != p && col != q)
+            {
+                temp[i][j++] = A[row][col];
+
+                // Row is filled, so increase row index and
+                // reset col index
+                if (j == n - 1)
+                {
+                    j = 0;
+                    i++;
+                }
+            }
+        }
+    }
+}
+
+/* Recursive function for finding determinant of matrix.
    n is current dimension of A[][]. */
-int determinant(int A[N][N], int n) 
-{ 
-    int D = 0; // Initialize result 
-  
-    //  Base case : if matrix contains single element 
-    if (n == 1) 
-        return A[0][0]; 
-  
-    int temp[N][N]; // To store cofactors 
-  
-    int sign = 1;  // To store sign multiplier 
-  
-     // Iterate for each element of first row 
-    for (int f = 0; f < n; f++) 
-    { 
-        // Getting Cofactor of A[0][f] 
-        getCofactor(A, temp, 0, f, n); 
-        D += sign * A[0][f] * determinant(temp, n - 1); 
-  
-        // terms are to be added with alternate sign 
-        sign = -sign; 
-    } 
-  
-    return D; 
-} 
-  
-// Function to get adjoint of A[N][N] in adj[N][N]. 
-void adjoint(int A[N][N],int adj[N][N]) 
-{ 
-    if (N == 1) 
-    { 
-        adj[0][0] = 1; 
-        return; 
-    } 
-  
-    // temp is used to store cofactors of A[][] 
-    int sign = 1, temp[N][N]; 
-  
-    for (int i=0; i<N; i++) 
-    { 
-        for (int j=0; j<N; j++) 
-        { 
-            // Get cofactor of A[i][j] 
-            getCofactor(A, temp, i, j, N); 
-  
-            // sign of adj[j][i] positive if sum of row 
-            // and column indexes is even. 
-            sign = ((i+j)%2==0)? 1: -1; 
-  
-            // Interchanging rows and columns to get the 
-            // transpose of the cofactor matrix 
-            adj[j][i] = (sign)*(determinant(temp, N-1)); 
-        } 
-    } 
-} 
-  
-// Function to calculate and store inverse, returns false if 
-// matrix is singular 
-bool inverse(int A[N][N], float inverse[N][N]) 
-{ 
-    // Find determinant of A[][] 
-    int det = determinant(A, N); 
-    if (det == 0) 
-    { 
-        cout << "Singular matrix, can't find its inverse"; 
-        return false; 
-    } 
-  
-    // Find adjoint 
-    int adj[N][N]; 
-    adjoint(A, adj); 
-  
-    // Find Inverse using formula "inverse(A) = adj(A)/det(A)" 
-    for (int i=0; i<N; i++) 
-        for (int j=0; j<N; j++) 
-            inverse[i][j] = adj[i][j]/float(det); 
-  
-    return true; 
-} 
+int determinant(int A[2][2], int n)
+{
+    int D = 0; // Initialize result
+
+    //  Base case : if matrix contains single element
+    if (n == 1)
+        return A[0][0];
+
+    int temp[2][2]; // To store cofactors
+
+    int sign = 1;  // To store sign multiplier
+
+     // Iterate for each element of first row
+    for (int f = 0; f < n; f++)
+    {
+        // Getting Cofactor of A[0][f]
+        getCofactor(A, temp, 0, f, n);
+        D += sign * A[0][f] * determinant(temp, n - 1);
+
+        // terms are to be added with alternate sign
+        sign = -sign;
+    }
+
+    return D;
+}
+
+// Function to get adjoint of A[N][N] in adj[N][N].
+void adjoint(int A[2][2],int adj[2][2], int N)
+{
+    if (N == 1)
+    {
+        adj[0][0] = 1;
+        return;
+    }
+
+    // temp is used to store cofactors of A[][]
+    int sign = 1, temp[N][N];
+
+    for (int i=0; i<N; i++)
+    {
+        for (int j=0; j<N; j++)
+        {
+            // Get cofactor of A[i][j]
+            getCofactor(A, temp, i, j, N);
+
+            // sign of adj[j][i] positive if sum of row
+            // and column indexes is even.
+            sign = ((i+j)%2==0)? 1: -1;
+
+            // Interchanging rows and columns to get the
+            // transpose of the cofactor matrix
+            adj[j][i] = (sign)*(determinant(temp, N-1));
+        }
+    }
+}
+
+// Function to calculate and store inverse, returns false if
+// matrix is singular
+bool inverse(int A[2][2], float inverse[2][2])
+{
+    // Find determinant of A[][]
+    int det = determinant(A, 2);
+    if (det == 0)
+    {
+        //cout << "Singular matrix, can't find its inverse";
+        return false;
+    }
+
+    // Find adjoint
+    int adj[2][2];
+    adjoint(A, adj, 2);
+
+    // Find Inverse using formula "inverse(A) = adj(A)/det(A)"
+    for (int i=0; i<2; i++)
+        for (int j=0; j<2; j++)
+            inverse[i][j] = adj[i][j]/float(det);
+
+    return true;
+}
 
 /**************************************************************************************************/
 
 /*************************MATRIX MULTIPLICATION****************************************************/
 
-//#define N 4 
-  
-// This function multiplies  
-// mat1[][] and mat2[][], and  
-// stores the result in res[][] 
-void multiply(int mat1[][N],  
-              int mat2[][N],  
-              int res[][N]) 
-{ 
-    int i, j, k; 
-    for (i = 0; i < N; i++) 
-    { 
-        for (j = 0; j < N; j++) 
-        { 
-            res[i][j] = 0; 
-            for (k = 0; k < N; k++) 
-                res[i][j] += mat1[i][k] *  
-                             mat2[k][j]; 
-        } 
-    } 
-} 
+//#define N 4
+
+// This function multiplies
+// mat1[][] and mat2[][], and
+// stores the result in res[][]
+void multiply(int mat1[][window],
+              int mat2[][2],
+              int res[2][2])
+{
+    int i, j, k;
+    for (i = 0; i < 2; i++)
+    {
+        for (j = 0; j < 2; j++)
+        {
+            res[i][j] = 0;
+            for (k = 0; k < window; k++)
+                res[i][j] += mat1[i][k] *
+                             mat2[k][j];
+        }
+    }
+}
 
 /**************************************************************************************************/
 
@@ -197,7 +198,44 @@ void multiply(int mat1[][N],
 // pass int status[corner_count] = {0};
 //pass the matrices for both frames;
 
-void opticalFlow(unsigned char ref[H][W], unsigned char next[H][W], int inputquad[corner_count][2], int outputquad[corner_count][2], int status[corner_count])
-{   
+void opticalFlow(unsigned char ref[H][W], unsigned char next[H][W], int inputquad[corner_count][2], int outputquad[corner_count][2], int status[corner_count], String err)
+{
+
+    int Ix[window] ={0};
+    int Iy[window] ={0};
+    int It[window] ={0}; // also the t vector of the equation
+
+    int i =0;
+    while(i< corner_count)
+    {
+      // (u v)t = (StS)^-1 * St. tvector
+      //calculate Ix[9] Iy[9] and It[9] for each corner
+
+      int S[window][2];
+
+      for(int i =0; i<window; i++)
+      S[i][1] = Ix[i];
+
+      for(int i =0; i<window; i++)
+      S[i][2] = Iy[i];
+
+      int ST[2][window]; // S transpose
+
+      transpose(S,ST);
+
+      int STS[2][2];// stores S transpose * S
+
+      multiply(ST,S,STS);
+
+      float STS_inverse[2][2];
+
+      if(!inverse(STS,STS_inverse)
+      {
+        err = "Couldn't find inverse of the singular matrix";
+        break;
+      }
+
+    }
+
     return 0;
 }
