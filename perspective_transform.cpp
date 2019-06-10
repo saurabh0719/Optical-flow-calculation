9f0eeebb82f143bfad777e5078f358b28421d1ec
saurabh0719 <saurabhpuj99@gmail.com>
2019-06-11 00:30:21 +0530

    New updates 2

--- perspective_transform.cpp
+++ perspective_transform.cpp
@@ -12,6 +12,150 @@ dst(i)=(x′i,y′i),src(i)=(xi,yi),i=0,1,2,3 */
 
 //top-left , top right, bottom-right, bottom-left
 
+#define corner_count 4
+
+/**************INVERSE OF A MATRIX*****************************************************************/
+
+// Function to get cofactor of A[p][q] in temp[][]. n is current
+// dimension of A[][]
+void getCofactor(int A[2][2], int temp[2][2], int p, int q, int n)
+{
+    int i = 0, j = 0;
+
+    // Looping for each element of the matrix
+    for (int row = 0; row < n; row++)
+    {
+        for (int col = 0; col < n; col++)
+        {
+            //  Copying into temporary matrix only those element
+            //  which are not in given row and column
+            if (row != p && col != q)
+            {
+                temp[i][j++] = A[row][col];
+
+                // Row is filled, so increase row index and
+                // reset col index
+                if (j == n - 1)
+                {
+                    j = 0;
+                    i++;
+                }
+            }
+        }
+    }
+}
+
+/* Recursive function for finding determinant of matrix.
+   n is current dimension of A[][]. */
+int determinant(int A[2][2], int n)
+{
+    int D = 0; // Initialize result
+
+    //  Base case : if matrix contains single element
+    if (n == 1)
+        return A[0][0];
+
+    int temp[2][2]; // To store cofactors
+
+    int sign = 1;  // To store sign multiplier
+
+     // Iterate for each element of first row
+    for (int f = 0; f < n; f++)
+    {
+        // Getting Cofactor of A[0][f]
+        getCofactor(A, temp, 0, f, n);
+        D += sign * A[0][f] * determinant(temp, n - 1);
+
+        // terms are to be added with alternate sign
+        sign = -sign;
+    }
+
+    return D;
+}
+
+// Function to get adjoint of A[N][N] in adj[N][N].
+void adjoint(int A[2][2],int adj[2][2], int N)
+{
+    if (N == 1)
+    {
+        adj[0][0] = 1;
+        return;
+    }
+
+    // temp is used to store cofactors of A[][]
+    int sign = 1, temp[N][N];
+
+    for (int i=0; i<N; i++)
+    {
+        for (int j=0; j<N; j++)
+        {
+            // Get cofactor of A[i][j]
+            getCofactor(A, temp, i, j, N);
+
+            // sign of adj[j][i] positive if sum of row
+            // and column indexes is even.
+            sign = ((i+j)%2==0)? 1: -1;
+
+            // Interchanging rows and columns to get the
+            // transpose of the cofactor matrix
+            adj[j][i] = (sign)*(determinant(temp, N-1));
+        }
+    }
+}
+
+// Function to calculate and store inverse, returns false if
+// matrix is singular
+bool inverse(int A[2][2], float inverse[2][2])
+{
+    // Find determinant of A[][]
+    int det = determinant(A, 2);
+    if (det == 0)
+    {
+        //cout << "Singular matrix, can't find its inverse";
+        return false;
+    }
+
+    // Find adjoint
+    int adj[2][2];
+    adjoint(A, adj, 2);
+
+    // Find Inverse using formula "inverse(A) = adj(A)/det(A)"
+    for (int i=0; i<2; i++)
+        for (int j=0; j<2; j++)
+            inverse[i][j] = adj[i][j]/float(det);
+
+    return true;
+}
+
+/**************************************************************************************************/
+
+
+/*************************MATRIX MULTIPLICATION****************************************************/
+
+//#define N 4
+
+// This function multiplies
+// mat1[][] and mat2[][], and
+// stores the result in res[][]
+void multiply(int mat1[][window],
+              int mat2[][2],
+              int res[2][2])
+{
+    int i, j, k;
+    for (i = 0; i < 2; i++)
+    {
+        for (j = 0; j < 2; j++)
+        {
+            res[i][j] = 0;
+            for (k = 0; k < window; k++)
+                res[i][j] += mat1[i][k] *
+                             mat2[k][j];
+        }
+    }
+}
+
+/**************************************************************************************************/
+
 void perspective_transform(int inputquad[corner_count][2], int outputquad[corner_count][2])
 {
   int U[8] = {0};
@@ -49,5 +193,7 @@ void perspective_transform(int inputquad[corner_count][2], int outputquad[corner
   }
 
   //finding inverse of X matrix
+
+
 
 }
