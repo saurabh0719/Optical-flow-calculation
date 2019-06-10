e9ad86b6af451c1f619a7c4505cbe3596c4b55fe
saurabh0719 <saurabhpuj99@gmail.com>
2019-06-11 00:17:07 +0530

    New updates

--- perspective_transform.cpp
+++ perspective_transform.cpp
@@ -9,3 +9,45 @@ The function calculates the 3×3 matrix of a perspective transform so that:
 where
 
 dst(i)=(x′i,y′i),src(i)=(xi,yi),i=0,1,2,3 */
+
+//top-left , top right, bottom-right, bottom-left
+
+void perspective_transform(int inputquad[corner_count][2], int outputquad[corner_count][2])
+{
+  int U[8] = {0};
+  int C[8] = {0}; //trans mat
+  int X[8][8] = {0};
+
+  // constructing the matrices
+  for(int i = 0; i<corner_count; i++)
+  U[i] = outputquad[i][0];
+  for(int i = 0; i<corner_count; i++)
+  U[i+4] = outputquad[i][1];
+
+  for(int i = 0; i<corner_count; i++)
+  {
+    X[i][0] = inputquad[i][0];
+    X[i][1] = inputquad[i][1];
+
+    X[i+4][3] = inputquad[i][0];
+    X[i+4][4] = inputquad[i][1];
+  }
+
+  for(int i = 0; i<corner_count; i++)
+  {
+    X[i][2] = 1;
+    X[i+4][5] = 1;
+  }
+
+  for(int i = 0; i<corner_count; i++)
+  {
+    X[i][6] = -X[i][0]*U[i];
+    X[i][7] = -X[i][1]*U[i];
+
+    X[i+4][6] = -X[i+4][3]*U[i+4];
+    X[i+4][7] = -X[i+4][4]*U[i+4];
+  }
+
+  //finding inverse of X matrix
+
+}
